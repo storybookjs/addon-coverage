5563c95b49c5c14ec49c93ac88aa7a142295990f
Yann Braga <yannbf@gmail.com>
2022-10-06 12:07:57 +0200

    chore: move examples to a separate folder

---
+++ examples/babel/.storybook/preview.js
@@ -0,0 +1,11 @@
+console.log(globalThis.__coverage__)
+
+export const parameters = {
+  actions: { argTypesRegex: "^on[A-Z].*" },
+  controls: {
+    matchers: {
+      color: /(background|color)$/i,
+      date: /Date$/,
+    },
+  },
+}
