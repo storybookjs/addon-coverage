458faa4562f31fe416af49d44b296cf4b266db4f
Yann Braga <yannbf@gmail.com>
2022-10-07 12:47:30 +0200

    add vite support

--- src/preset.ts
+++ src/preset.ts
@@ -3,20 +3,44 @@ import type { Options } from "@storybook/core-common";
 import { defaultExclude, defaultExtensions } from "./constants";
 import type { AddonOptions } from "./types";
 
+export const viteFinal = async (
+  viteConfig: Record<string, any>,
+  options: Options & AddonOptions
+) => {
+  const istanbul = require("vite-plugin-istanbul");
+  console.log("Adding istanbul plugin to vite config");
+  viteConfig.plugins ||= [];
+  viteConfig.plugins.push(
+    istanbul({
+      ...options.istanbul,
+      include: Array.from(options.istanbul?.include || []),
+      exclude: [
+        options.configDir + "/**",
+        ...defaultExclude,
+        ...Array.from(options.istanbul?.exclude || []),
+      ],
+      extension: options.istanbul?.extension || defaultExtensions,
+    })
+  );
+
+  return viteConfig;
+};
+
 export const babel = async (
   babelConfig: TransformOptions,
   options: Options & AddonOptions
 ) => {
+  console.log("Adding istanbul plugin to babel config");
   babelConfig.plugins ||= [];
   babelConfig.plugins.push([
     "istanbul",
     {
       ...options.istanbul,
-      include: options.istanbul?.include || [],
+      include: Array.from(options.istanbul?.include || []),
       exclude: [
         options.configDir + "/**",
         ...defaultExclude,
-        ...(options.istanbul?.exclude || []),
+        ...Array.from(options.istanbul?.exclude || []),
       ],
       extension: options.istanbul?.extension || defaultExtensions,
     },
