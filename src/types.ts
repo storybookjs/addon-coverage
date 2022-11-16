b681c3554487885db191ccf646465b385195662a
Bryan Thomas <49354825+bryanjtc@users.noreply.github.com>
2022-11-16 09:18:58 -0500

    fix: Divided addon options for babel and vite

--- src/types.ts
+++ src/types.ts
@@ -1,12 +1,21 @@
-import type { IstanbulPluginOptions } from "vite-plugin-istanbul";
-
-export interface IstanbulOptions extends IstanbulPluginOptions {
+import type { IstanbulPluginOptions as AddonOptionsVite } from "vite-plugin-istanbul";
+import type { FileCoverage } from 'istanbul-lib-coverage';
+export interface AddonOptionsBabel {
   cwd?: string;
   include?: string[];
   exclude?: string[];
   extension?: string[];
+  excludeNodeModules?: boolean;
+  coverageVariable?: string;
+  coverageGlobalScopeFunc?: boolean,
+  ignoreClassMethods?: string[]
+  useInlineSourceMaps?: boolean;
+  inputSourceMap?: object;
+  nycrcPath?: string;
+  onCover?: (fileName: string, fileCoverage: FileCoverage) => unknown;
+  fileName?: string;
 }
 
 export interface AddonOptions {
-  istanbul: IstanbulOptions;
+  istanbul: AddonOptionsBabel | AddonOptionsVite;
 }
