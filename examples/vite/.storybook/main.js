b7a06e59e14a9d44364248ab3e2d8185b835650f
Yann Braga <yannbf@gmail.com>
2023-03-20 18:55:11 +0100

    migrate vite example to sb 7

---
+++ examples/vite/.storybook/main.js
@@ -0,0 +1,15 @@
+module.exports = {
+  stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
+  addons: [
+    "@storybook/addon-essentials",
+    "@storybook/addon-interactions",
+    "@storybook/addon-coverage",
+  ],
+  framework: {
+    name: "@storybook/react-vite",
+    options: {},
+  },
+  docs: {
+    autodocs: true,
+  },
+};
